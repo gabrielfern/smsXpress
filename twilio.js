184f0510f772bb6dced60fce1088b9037235f33f
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-12-20 20:54:51 -0300

    switching hardcoded information to env variables

--- twilio.js
+++ twilio.js
@@ -1,21 +1,21 @@
-const accountSid = 'ACce6239c8085f82e0b8ec8072163d90be'
-const authToken = 'a11720ead3467466f15af33490df57df'
+const accountSid = process.env.accountSid
+const authToken = process.env.authToken
 const client = require('twilio')(accountSid, authToken)
 
-const twilioNumber = '+5547933007071'
+const twilioNumber = process.env.twilioNumber
 
 function sendSms(message, callback) {
   if (message) {
     client.messages.create({
       body: message,
       from: twilioNumber,
-      to: '+5583987682063'
+      to: process.env.myNumber
     })
       .then(res => {
         if (callback)
           callback(res.sid)
        })
-      .done()  
+      .done()
   }
 }
 
